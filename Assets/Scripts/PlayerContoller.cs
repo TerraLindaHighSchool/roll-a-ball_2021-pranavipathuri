76fd31fd627df55aa45a1d1d5fb64669e3f52229
pranavipathuri <pp42496@srcs.org>
2021-12-02 17:45:45 -0600

    Moving a Player 2.6

---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerContoller : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>;
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.get<Vector2>;
+    }
+}
