a9e03398462b2ae920b7b0a9c3882649649d33ff
pranavipathuri <pp42496@srcs.org>
2021-12-09 17:46:16 -0600

    More done Roll a Ball

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 public class PlayerContoller : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
     }
 
     private void OnMove(InputValue movementValue)
@@ -23,10 +30,26 @@ public class PlayerContoller : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+
     }
 }
